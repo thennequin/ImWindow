2740c32032b4e116cf9c3f7a5c0391ad86ba7c66
CameleonTH <cameleonth@gmail.com>
2015-09-26 21:10:04 +0200

    Increase log buffer size

--- ImWindow/ImwWindowManager.cpp
+++ ImWindow/ImwWindowManager.cpp
@@ -175,6 +175,7 @@ ImwPlatformWindow* ImwWindowManager::GetWindowParent(ImwWindow* pWindow)
 void ImwWindowManager::Log(const char* pFormat, ...)
 {
 	char pBuffer[2048];
+	char pBuffer[32768];
 	va_list argptr;
 	va_start(argptr, pFormat);
 	vsprintf_s(pBuffer, sizeof(char) * 2047, pFormat, argptr);
