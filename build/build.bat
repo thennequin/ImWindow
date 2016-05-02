befaeb0020cf5e1a53854a79f8863aec0b745a43
Thibault Hennequin <cameleonth@gmail.com>
2016-05-02 18:56:35 +0200

    Add message in build.bat when no argument is set

--- build/build.bat
+++ build/build.bat
@@ -5,4 +5,10 @@ if not exist "genie.exe" (
 	bitsadmin /transfer "genie" "https://github.com/bkaradzic/bx/raw/master/tools/bin/windows/genie.exe" "%~dp0\genie.exe"
 )
 
+IF "%1"=="" (
+	echo No argument set
+	echo Exemple : build.bat vs2015
+	exit 1
+)
+
 genie.exe %*
