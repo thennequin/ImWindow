81b47bfd48c027083291003ee866be08d96dde5b
Thibault Hennequin <cameleonth@gmail.com>
2017-06-13 12:07:28 +0200

    Use powershell instead of bitsadmin for download genie.exe

--- scripts/build.bat
+++ scripts/build.bat
@@ -2,7 +2,8 @@
 
 if not exist "genie.exe" (
 	echo Downloading genie...
-	bitsadmin /transfer "genie" "https://github.com/bkaradzic/bx/raw/master/tools/bin/windows/genie.exe" "%~dp0\genie.exe"
+	rem bitsadmin /transfer "genie" "https://github.com/bkaradzic/bx/raw/master/tools/bin/windows/genie.exe" "%~dp0\genie.exe"
+	powershell Invoke-WebRequest -Uri "https://github.com/bkaradzic/bx/raw/master/tools/bin/windows/genie.exe" -OutFile "%~dp0\genie.exe"
 )
 
 IF "%1"=="" (
