0616a65bb58ff4b129c495e2d1dff3d376b781a9
CameleonTH <cameleonth@gmail.com>
2017-08-06 21:10:04 +0200

    Fix build script when project path has spaces

--- scripts/build.bat
+++ scripts/build.bat
@@ -3,7 +3,7 @@
 if not exist "genie.exe" (
 	echo Downloading genie...
 	rem bitsadmin /transfer "genie" "https://github.com/bkaradzic/bx/raw/master/tools/bin/windows/genie.exe" "%~dp0\genie.exe"
-	powershell Invoke-WebRequest -Uri "https://github.com/bkaradzic/bx/raw/master/tools/bin/windows/genie.exe" -OutFile "%~dp0\genie.exe"
+	powershell Invoke-WebRequest -Uri "https://github.com/bkaradzic/bx/raw/master/tools/bin/windows/genie.exe" -OutFile '%~dp0\genie.exe'
 )
 
 IF "%1"=="" (
