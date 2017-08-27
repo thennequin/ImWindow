aa3f514d938f4560dc8f7540f25c68f70692f7bb
Thibault Hennequin <cameleonth@gmail.com>
2017-08-27 16:48:08 +0200

    Move Node window is sample.h

--- sample.h
+++ sample.h
@@ -17,173 +17,8 @@
 
 using namespace ImWindow;
 
-class MyStatusBar : public ImwStatusBar
-{
-public:
-	MyStatusBar()
-	{
-		m_fTime = 0.f;
-	}
-
-	virtual void OnStatusBar()
-	{
-		m_fTime += ImGui::GetIO().DeltaTime;
-		ImGui::Text("My status bar");
-		ImGui::SameLine();
-		while (m_fTime > 5.f)
-			m_fTime -= 5.f;
-		ImGui::ProgressBar(m_fTime / 5.f, ImVec2(150.f, 0.f));
-	}
-
-	float m_fTime;
-};
-
-class MyToolBar : public ImwToolBar
-{
-public:
-	virtual void OnToolBar()
-	{
-		ImGui::Text("My tool bar");
-		ImGui::SameLine();
-		ImGui::SmallButton("Button");
-	}
-};
-
-class MyMenu : public ImwMenu
-{
-public:
-	MyMenu()
-		: ImwMenu(-1)
-	{
-	}
-
-	virtual void OnMenu()
-	{
-		if (ImGui::BeginMenu("My menu"))
-		{
-			if (ImGui::MenuItem("Show content", NULL, ImWindow::ImwWindowManager::GetInstance()->GetMainPlatformWindow()->IsShowContent()))
-			{
-				ImWindow::ImwWindowManager::GetInstance()->GetMainPlatformWindow()->SetShowContent(!ImWindow::ImwWindowManager::GetInstance()->GetMainPlatformWindow()->IsShowContent());
-			}
-
-			ImGui::Separator();
-
-			if (ImGui::MenuItem("Exit"))
-			{
-				ImWindow::ImwWindowManager::GetInstance()->Destroy();
-			}
-
-			ImGui::EndMenu();
-		}
-	}
-};
-
-class MyImwWindow3 : public ImwWindow
-{
-public:
-	MyImwWindow3(const char* pTitle = "MyImwWindow3")
-	{
-		SetTitle(pTitle);
-	}
-
-	virtual void OnGui()
-	{
-		if (!IsClosable())
-		{
-			ImGui::TextWrapped("I'm not closeable.\n");
-			ImGui::PushStyleColor(ImGuiCol_Text, ImGui::GetStyle().Colors[ImGuiCol_TextDisabled]);
-			ImGui::TextWrapped("So you can't close the platform window\nuntil closing me.");
-			ImGui::PopStyleColor();
-			ImGui::TextWrapped("For close me, click on button 'Close'");
-			ImGui::Separator();
-		}
-
-		ImGui::TextWrapped("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-
-		if (ImGui::Button("Close"))
-		{
-			Destroy();
-		}
-	}
-};
-
-class MyImwWindow2 : public ImwWindow
-{
-public:
-	MyImwWindow2(const char* pTitle = "MyImwWindow2")
-	{
-		SetTitle(pTitle);
-	}
-
-	virtual void OnGui()
-	{
-		static float f = 0.0f;
-		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
-	}
-};
-
-class MyImwWindowFillSpace : public ImwWindow
-{
-public:
-	MyImwWindowFillSpace()
-	{
-		SetTitle("Filling space");
-		SetFillingSpace(true);
-	}
-
-	virtual void OnGui()
-	{
-		ImVec2 oSize = ImGui::GetContentRegionAvail();
-		ImGui::Selectable("I'm a selectable\nwho fills the whole space", true, 0, oSize);
-	}
-};
-
-class MyImwWindow : public ImwWindow, ImwMenu
-{
-public:
-	MyImwWindow(const char* pTitle = "MyImwWindow")
-		: ImwMenu(0, false)
-	{
-		SetTitle(pTitle);
-		SetAlone(true);
-		m_pText[0] = 0;
-	}
-	virtual void OnGui()
-	{
-		ImGui::Text("Hello, world! I'm an alone window");
-
-		if (ImGui::Button("Create new MyImwWindow3"))
-		{
-			new MyImwWindow3();
-		}
-
-		ImGui::InputText("Input", m_pText, 512);
-
-		//ImGui::ShowMetricsWindow();
-	}
-
-	virtual void OnContextMenu()
-	{
-		if (ImGui::MenuItem("Focus"))
-		{
-			ImwWindowManager::GetInstance()->FocusWindow(this);
-		}
-	}
-
-	virtual void OnMenu()
-	{
-		if (ImGui::BeginMenu("MyImwWindow"))
-		{
-			if (ImGui::MenuItem("Create new MyImwWindow3"))
-			{
-				new MyImwWindow3();
-			}
-			ImGui::EndMenu();
-		}
-	}
-
-	char m_pText[512];
-};
+static inline ImVec2 operator+(const ImVec2& lhs, const ImVec2& rhs) { return ImVec2(lhs.x + rhs.x, lhs.y + rhs.y); }
+static inline ImVec2 operator-(const ImVec2& lhs, const ImVec2& rhs) { return ImVec2(lhs.x - rhs.x, lhs.y - rhs.y); }
 
 class StyleEditorWindow : public ImwWindow
 {
@@ -329,6 +164,392 @@ public:
 
 		ImGui::PopItemWidth();
 	}
+};
+
+
+class NodeWindow : public ImWindow::ImwWindow
+{
+public:
+	NodeWindow(void)
+	{
+		SetTitle("Node editor");
+	}
+
+	virtual void OnGui()
+	{
+		// Dummy
+		struct Node
+		{
+			int     ID;
+			char    Name[32];
+			ImVec2  Pos, Size;
+			float   Value;
+			ImVec4  Color;
+			int     InputsCount, OutputsCount;
+
+			Node(int id, const char* name, const ImVec2& pos, float value, const ImVec4& color, int inputs_count, int outputs_count) { ID = id; strncpy(Name, name, 31); Name[31] = 0; Pos = pos; Value = value; Color = color; InputsCount = inputs_count; OutputsCount = outputs_count; }
+
+			ImVec2 GetInputSlotPos(int slot_no) const { return ImVec2(Pos.x, Pos.y + Size.y * ((float)slot_no + 1) / ((float)InputsCount + 1)); }
+			ImVec2 GetOutputSlotPos(int slot_no) const { return ImVec2(Pos.x + Size.x, Pos.y + Size.y * ((float)slot_no + 1) / ((float)OutputsCount + 1)); }
+		};
+		struct NodeLink
+		{
+			int     InputIdx, InputSlot, OutputIdx, OutputSlot;
+
+			NodeLink(int input_idx, int input_slot, int output_idx, int output_slot) { InputIdx = input_idx; InputSlot = input_slot; OutputIdx = output_idx; OutputSlot = output_slot; }
+		};
+
+		static ImVector<Node> nodes;
+		static ImVector<NodeLink> links;
+		static bool inited = false;
+		static ImVec2 scrolling = ImVec2(0.0f, 0.0f);
+		static bool show_grid = true;
+		static int node_selected = -1;
+		if (!inited)
+		{
+			nodes.push_back(Node(0, "MainTex", ImVec2(40, 50), 0.5f, ImColor(255, 100, 100), 1, 1));
+			nodes.push_back(Node(1, "BumpMap", ImVec2(40, 150), 0.42f, ImColor(200, 100, 200), 1, 1));
+			nodes.push_back(Node(2, "Combine", ImVec2(270, 80), 1.0f, ImColor(0, 200, 100), 2, 2));
+			links.push_back(NodeLink(0, 0, 2, 0));
+			links.push_back(NodeLink(1, 0, 2, 1));
+			inited = true;
+		}
+
+		// Draw a list of nodes on the left side
+		bool open_context_menu = false;
+		int node_hovered_in_list = -1;
+		int node_hovered_in_scene = -1;
+		ImGui::BeginChild("node_list", ImVec2(100, 0));
+		ImGui::Text("Nodes");
+		ImGui::Separator();
+		for (int node_idx = 0; node_idx < nodes.Size; node_idx++)
+		{
+			Node* node = &nodes[node_idx];
+			ImGui::PushID(node->ID);
+			if (ImGui::Selectable(node->Name, node->ID == node_selected))
+				node_selected = node->ID;
+			if (ImGui::IsItemHovered())
+			{
+				node_hovered_in_list = node->ID;
+				open_context_menu |= ImGui::IsMouseClicked(1);
+			}
+			ImGui::PopID();
+		}
+		ImGui::EndChild();
+
+		ImGui::SameLine();
+		ImGui::BeginGroup();
+
+		const float NODE_SLOT_RADIUS = 4.0f;
+		const ImVec2 NODE_WINDOW_PADDING(8.0f, 8.0f);
+
+		// Create our child canvas
+		ImGui::Text("Hold middle mouse button to scroll (%.2f,%.2f)", scrolling.x, scrolling.y);
+		ImGui::SameLine(ImGui::GetWindowWidth() - 100);
+		ImGui::Checkbox("Show grid", &show_grid);
+		ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(1, 1));
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+		ImGui::PushStyleColor(ImGuiCol_ChildWindowBg, ImColor(60, 60, 70, 200));
+		ImGui::BeginChild("scrolling_region", ImVec2(0, 0), true, ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoMove);
+		ImGui::PushItemWidth(120.0f);
+
+		ImVec2 offset = ImGui::GetCursorScreenPos() - scrolling;
+		ImDrawList* draw_list = ImGui::GetWindowDrawList();
+		draw_list->ChannelsSplit(2);
+
+		// Display grid
+		if (show_grid)
+		{
+			ImU32 GRID_COLOR = ImColor(120, 120, 120, 40);
+			float GRID_SZ = 32.0f;
+			ImVec2 win_pos = ImGui::GetCursorScreenPos();
+			ImVec2 canvas_sz = ImGui::GetWindowSize();
+			for (float x = fmodf(offset.x, GRID_SZ); x < canvas_sz.x; x += GRID_SZ)
+				draw_list->AddLine(ImVec2(x, 0.0f) + win_pos, ImVec2(x, canvas_sz.y) + win_pos, GRID_COLOR);
+			for (float y = fmodf(offset.y, GRID_SZ); y < canvas_sz.y; y += GRID_SZ)
+				draw_list->AddLine(ImVec2(0.0f, y) + win_pos, ImVec2(canvas_sz.x, y) + win_pos, GRID_COLOR);
+		}
+
+		// Display links
+		draw_list->ChannelsSetCurrent(0); // Background
+		for (int link_idx = 0; link_idx < links.Size; link_idx++)
+		{
+			NodeLink* link = &links[link_idx];
+			Node* node_inp = &nodes[link->InputIdx];
+			Node* node_out = &nodes[link->OutputIdx];
+			ImVec2 p1 = offset + node_inp->GetOutputSlotPos(link->InputSlot);
+			ImVec2 p2 = offset + node_out->GetInputSlotPos(link->OutputSlot);
+			draw_list->AddBezierCurve(p1, p1 + ImVec2(+50, 0), p2 + ImVec2(-50, 0), p2, ImColor(200, 200, 100), 3.0f);
+		}
+
+		// Display nodes
+		for (int node_idx = 0; node_idx < nodes.Size; node_idx++)
+		{
+			Node* node = &nodes[node_idx];
+			ImGui::PushID(node->ID);
+			ImVec2 node_rect_min = offset + node->Pos;
+
+			// Display node contents first
+			draw_list->ChannelsSetCurrent(1); // Foreground
+			bool old_any_active = ImGui::IsAnyItemActive();
+			ImGui::SetCursorScreenPos(node_rect_min + NODE_WINDOW_PADDING);
+			ImGui::BeginGroup(); // Lock horizontal position
+			ImGui::Text("%s", node->Name);
+			ImGui::SliderFloat("##value", &node->Value, 0.0f, 1.0f, "Alpha %.2f");
+			ImGui::ColorEdit3("##color", &node->Color.x);
+			ImGui::EndGroup();
+
+			// Save the size of what we have emitted and whether any of the widgets are being used
+			bool node_widgets_active = (!old_any_active && ImGui::IsAnyItemActive());
+			node->Size = ImGui::GetItemRectSize() + NODE_WINDOW_PADDING + NODE_WINDOW_PADDING;
+			ImVec2 node_rect_max = node_rect_min + node->Size;
+
+			// Display node box
+			draw_list->ChannelsSetCurrent(0); // Background
+			ImGui::SetCursorScreenPos(node_rect_min);
+			ImGui::InvisibleButton("node", node->Size);
+			if (ImGui::IsItemHovered())
+			{
+				node_hovered_in_scene = node->ID;
+				open_context_menu |= ImGui::IsMouseClicked(1);
+			}
+			bool node_moving_active = ImGui::IsItemActive();
+			if (node_widgets_active || node_moving_active)
+				node_selected = node->ID;
+			if (node_moving_active && ImGui::IsMouseDragging(0))
+				node->Pos = node->Pos + ImGui::GetIO().MouseDelta;
+
+			ImU32 node_bg_color = (node_hovered_in_list == node->ID || node_hovered_in_scene == node->ID || (node_hovered_in_list == -1 && node_selected == node->ID)) ? ImColor(75, 75, 75) : ImColor(60, 60, 60);
+			draw_list->AddRectFilled(node_rect_min, node_rect_max, node_bg_color, 3.0f);
+			draw_list->AddRect(node_rect_min, node_rect_max, ImColor(100, 100, 100), 3.0f);
+			for (int slot_idx = 0; slot_idx < node->InputsCount; slot_idx++)
+				draw_list->AddCircleFilled(offset + node->GetInputSlotPos(slot_idx), NODE_SLOT_RADIUS, ImColor(150, 150, 150, 150));
+			for (int slot_idx = 0; slot_idx < node->OutputsCount; slot_idx++)
+				draw_list->AddCircleFilled(offset + node->GetOutputSlotPos(slot_idx), NODE_SLOT_RADIUS, ImColor(150, 150, 150, 150));
+
+			ImGui::PopID();
+		}
+		draw_list->ChannelsMerge();
+
+		// Open context menu
+		if (!ImGui::IsAnyItemHovered() && ImGui::IsMouseHoveringWindow() && ImGui::IsMouseClicked(1))
+		{
+			node_selected = node_hovered_in_list = node_hovered_in_scene = -1;
+			open_context_menu = true;
+		}
+		if (open_context_menu)
+		{
+			ImGui::OpenPopup("context_menu");
+			if (node_hovered_in_list != -1)
+				node_selected = node_hovered_in_list;
+			if (node_hovered_in_scene != -1)
+				node_selected = node_hovered_in_scene;
+		}
+
+		// Draw context menu
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(8, 8));
+		if (ImGui::BeginPopup("context_menu"))
+		{
+			Node* node = node_selected != -1 ? &nodes[node_selected] : NULL;
+			ImVec2 scene_pos = ImGui::GetMousePosOnOpeningCurrentPopup() - offset;
+			if (node)
+			{
+				ImGui::Text("Node '%s'", node->Name);
+				ImGui::Separator();
+				if (ImGui::MenuItem("Rename..", NULL, false, false)) {}
+				if (ImGui::MenuItem("Delete", NULL, false, false)) {}
+				if (ImGui::MenuItem("Copy", NULL, false, false)) {}
+			}
+			else
+			{
+				if (ImGui::MenuItem("Add")) { nodes.push_back(Node(nodes.Size, "New node", scene_pos, 0.5f, ImColor(100, 100, 200), 2, 2)); }
+				if (ImGui::MenuItem("Paste", NULL, false, false)) {}
+			}
+			ImGui::EndPopup();
+		}
+		ImGui::PopStyleVar();
+
+		// Scrolling
+		if (ImGui::IsWindowHovered() && !ImGui::IsAnyItemActive() && ImGui::IsMouseDragging(2, 0.0f))
+			scrolling = scrolling - ImGui::GetIO().MouseDelta;
+
+		ImGui::PopItemWidth();
+		ImGui::EndChild();
+		ImGui::PopStyleColor();
+		ImGui::PopStyleVar(2);
+		ImGui::EndGroup();
+	}
+};
+
+class MyStatusBar : public ImwStatusBar
+{
+public:
+	MyStatusBar()
+	{
+		m_fTime = 0.f;
+	}
+
+	virtual void OnStatusBar()
+	{
+		m_fTime += ImGui::GetIO().DeltaTime;
+		ImGui::Text("My status bar");
+		ImGui::SameLine();
+		while (m_fTime > 5.f)
+			m_fTime -= 5.f;
+		ImGui::ProgressBar(m_fTime / 5.f, ImVec2(150.f, 0.f));
+	}
+
+	float m_fTime;
+};
+
+class MyToolBar : public ImwToolBar
+{
+public:
+	virtual void OnToolBar()
+	{
+		ImGui::Text("My tool bar");
+		ImGui::SameLine();
+		if (ImGui::SmallButton("Open Node window"))
+		{
+			new NodeWindow();
+		}
+	}
+};
+
+class MyMenu : public ImwMenu
+{
+public:
+	MyMenu()
+		: ImwMenu(-1)
+	{
+	}
+
+	virtual void OnMenu()
+	{
+		if (ImGui::BeginMenu("My menu"))
+		{
+			if (ImGui::MenuItem("Show content", NULL, ImWindow::ImwWindowManager::GetInstance()->GetMainPlatformWindow()->IsShowContent()))
+			{
+				ImWindow::ImwWindowManager::GetInstance()->GetMainPlatformWindow()->SetShowContent(!ImWindow::ImwWindowManager::GetInstance()->GetMainPlatformWindow()->IsShowContent());
+			}
+
+			ImGui::Separator();
+
+			if (ImGui::MenuItem("Exit"))
+			{
+				ImWindow::ImwWindowManager::GetInstance()->Destroy();
+			}
+
+			ImGui::EndMenu();
+		}
+	}
+};
+
+class MyImwWindow3 : public ImwWindow
+{
+public:
+	MyImwWindow3(const char* pTitle = "MyImwWindow3")
+	{
+		SetTitle(pTitle);
+	}
+
+	virtual void OnGui()
+	{
+		if (!IsClosable())
+		{
+			ImGui::TextWrapped("I'm not closeable.\n");
+			ImGui::PushStyleColor(ImGuiCol_Text, ImGui::GetStyle().Colors[ImGuiCol_TextDisabled]);
+			ImGui::TextWrapped("So you can't close the platform window\nuntil closing me.");
+			ImGui::PopStyleColor();
+			ImGui::TextWrapped("For close me, click on button 'Close'");
+			ImGui::Separator();
+		}
+
+		ImGui::TextWrapped("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+
+		if (ImGui::Button("Close"))
+		{
+			Destroy();
+		}
+	}
+};
+
+class MyImwWindow2 : public ImwWindow
+{
+public:
+	MyImwWindow2(const char* pTitle = "MyImwWindow2")
+	{
+		SetTitle(pTitle);
+	}
+
+	virtual void OnGui()
+	{
+		static float f = 0.0f;
+		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+	}
+};
+
+class MyImwWindowFillSpace : public ImwWindow
+{
+public:
+	MyImwWindowFillSpace()
+	{
+		SetTitle("Filling space");
+		SetFillingSpace(true);
+	}
+
+	virtual void OnGui()
+	{
+		ImVec2 oSize = ImGui::GetContentRegionAvail();
+		ImGui::Selectable("I'm a selectable\nwho fills the whole space", true, 0, oSize);
+	}
+};
+
+class MyImwWindow : public ImwWindow, ImwMenu
+{
+public:
+	MyImwWindow(const char* pTitle = "MyImwWindow")
+		: ImwMenu(0, false)
+	{
+		SetTitle(pTitle);
+		SetAlone(true);
+		m_pText[0] = 0;
+	}
+	virtual void OnGui()
+	{
+		ImGui::Text("Hello, world! I'm an alone window");
+
+		if (ImGui::Button("Create new MyImwWindow3"))
+		{
+			new MyImwWindow3();
+		}
+
+		ImGui::InputText("Input", m_pText, 512);
+
+		//ImGui::ShowMetricsWindow();
+	}
+
+	virtual void OnContextMenu()
+	{
+		if (ImGui::MenuItem("Focus"))
+		{
+			ImwWindowManager::GetInstance()->FocusWindow(this);
+		}
+	}
+
+	virtual void OnMenu()
+	{
+		if (ImGui::BeginMenu("MyImwWindow"))
+		{
+			if (ImGui::MenuItem("Create new MyImwWindow3"))
+			{
+				new MyImwWindow3();
+			}
+			ImGui::EndMenu();
+		}
+	}
+
+	char m_pText[512];
 };
 
 void PreInitSample()
