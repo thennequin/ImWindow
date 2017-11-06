e0689ec6a0ff413252290437069f25958a042f2a
Thibault Hennequin <cameleonth@gmail.com>
2017-11-06 19:25:27 +0100

    Change the way to embed shader in code

--- ImWindowDX11/ImwPlatformWindowDX11.cpp
+++ ImWindowDX11/ImwPlatformWindowDX11.cpp
@@ -80,53 +80,55 @@ struct BACKUP_DX11_STATE
 	}
 };
 
-static const char* c_pVertexShader = R"***(
-cbuffer vertexBuffer : register(b0)
-{
-    float4x4 ProjectionMatrix;
-};
+#define SHADER_SOURCE(...) #__VA_ARGS__
 
-struct VS_INPUT
-{
-    float2 pos : POSITION;
-    float4 col : COLOR0;
-    float2 uv  : TEXCOORD0;
-};
+static const char* c_pVertexShader = SHADER_SOURCE(
+	cbuffer vertexBuffer : register(b0)
+	{
+		float4x4 ProjectionMatrix;
+	};
 
-struct PS_INPUT
-{
-    float4 pos : SV_POSITION;
-    float4 col : COLOR0;
-    float2 uv  : TEXCOORD0;
-};
+	struct VS_INPUT
+	{
+		float2 pos : POSITION;
+		float4 col : COLOR0;
+		float2 uv  : TEXCOORD0;
+	};
 
-PS_INPUT main(VS_INPUT input)
-{
-    PS_INPUT output;
-    output.pos = mul( ProjectionMatrix, float4(input.pos.xy, 0.f, 1.f));
-    output.col = input.col;
-    output.uv  = input.uv;
-    return output;
-}
-)***";
+	struct PS_INPUT
+	{
+		float4 pos : SV_POSITION;
+		float4 col : COLOR0;
+		float2 uv  : TEXCOORD0;
+	};
 
-static const char* c_pPixelShader = R"***(
-struct PS_INPUT
-{
-    float4 pos : SV_POSITION;
-    float4 col : COLOR0;
-    float2 uv  : TEXCOORD0;
-};
+	PS_INPUT main(VS_INPUT input)
+	{
+		PS_INPUT output;
+		output.pos = mul( ProjectionMatrix, float4(input.pos.xy, 0.f, 1.f));
+		output.col = input.col;
+		output.uv  = input.uv;
+		return output;
+	}
+);
 
-sampler sampler0;
-Texture2D texture0;
+static const char* c_pPixelShader = SHADER_SOURCE(
+	struct PS_INPUT
+	{
+		float4 pos : SV_POSITION;
+		float4 col : COLOR0;
+		float2 uv  : TEXCOORD0;
+	};
 
-float4 main(PS_INPUT input) : SV_Target
-{
-    float4 out_col = input.col * texture0.Sample(sampler0, input.uv); 
-    return out_col; 
-}
-)***";
+	sampler sampler0;
+	Texture2D texture0;
+
+	float4 main(PS_INPUT input) : SV_Target
+	{
+		float4 out_col = input.col * texture0.Sample(sampler0, input.uv); 
+		return out_col; 
+	}
+);
 
 using namespace ImWindow;
 
