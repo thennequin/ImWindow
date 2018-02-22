f634363814fb21f3b175fb0ddc6a3f6ff655116c
CameleonTH <cameleonth@gmail.com>
2018-02-22 23:55:22 +0100

    Try to refix download of genie

--- scripts/build.bat
+++ scripts/build.bat
@@ -3,7 +3,7 @@
 if not exist "genie.exe" (
 	echo Downloading genie...
 	rem bitsadmin /transfer "genie" "https://github.com/bkaradzic/bx/raw/master/tools/bin/windows/genie.exe" "%~dp0\genie.exe"
-	powershell $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'; [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols; Invoke-WebRequest -Uri "https://github.com/bkaradzic/bx/raw/master/tools/bin/windows/genie.exe" -OutFile '%~dp0\genie.exe'
+	powershell [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri "https://github.com/bkaradzic/bx/raw/master/tools/bin/windows/genie.exe" -OutFile '%~dp0\genie.exe'
 )
 
 IF "%1"=="" (
