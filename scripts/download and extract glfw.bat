9716881f01994d6c523b9e28c18bc6cae1e596ba
CameleonTH <cameleonth@gmail.com>
2018-03-18 00:35:49 +0100

    Fix script for download GLFW binaries

--- scripts/download and extract glfw.bat
+++ scripts/download and extract glfw.bat
@@ -2,11 +2,11 @@
 
 if not exist "%~dp0\glfw-3.2.1.bin.WIN32.zip" (
 	echo Downloading GLFW 3.2.1 Win32...
-	powershell Invoke-WebRequest -Uri "https://github.com/glfw/glfw/releases/download/3.2.1/glfw-3.2.1.bin.WIN32.zip" -OutFile '%~dp0\glfw-3.2.1.bin.WIN32.zip'
+	powershell [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-WebRequest -Uri "https://github.com/glfw/glfw/releases/download/3.2.1/glfw-3.2.1.bin.WIN32.zip" -OutFile '%~dp0\glfw-3.2.1.bin.WIN32.zip'
 )
 if not exist "%~dp0\glfw-3.2.1.bin.WIN64.zip" (
 	echo Downloading GLFW 3.2.1 WIN64...
-	powershell Invoke-WebRequest -Uri "https://github.com/glfw/glfw/releases/download/3.2.1/glfw-3.2.1.bin.WIN64.zip" -OutFile '%~dp0\glfw-3.2.1.bin.WIN64.zip'
+	powershell [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;Invoke-WebRequest -Uri "https://github.com/glfw/glfw/releases/download/3.2.1/glfw-3.2.1.bin.WIN64.zip" -OutFile '%~dp0\glfw-3.2.1.bin.WIN64.zip'
 )
 
 echo Extracting GLFW 3.2.1 Win32...
